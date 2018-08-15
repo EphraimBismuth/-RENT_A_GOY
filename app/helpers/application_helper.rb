fae8cbba46bfc3fd1e203734eff4c854cd7eccc3
Benjamin Aronov <ruskibear31@gmail.com>
2018-08-15 14:07:39 +0300

    updated goy model and show to implement availability

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
     )
     super(number, options)
   end
+
+  def booking_link(date)
+    if @goy.booked_on?(date)
+      link_to 'Booked', "#", class: 'btn btn-gag', disabled: true
+    else
+      link_to 'Available', goy_bookings_path(@goy, booking: {start_date: date, end_date: date}), method: :post, class: 'btn btn-gag'
+    end
+  end
 end
