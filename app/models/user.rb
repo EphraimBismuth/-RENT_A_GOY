a3964db443453dc90d7044896c3bbeb2e2946507
Ephraim Bismuth <ephraim_bismuth@hotmail.fr>
2018-08-15 12:28:54 +0300

    adding links to navbar and setting sign in/up/out

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  validates :type, presence: :true, inclusion: {in: ["Goy", "Jew"]}
+  validates :type, presence: :true, inclusion: {in: ["Goy", "Jew"] }
 
 end
