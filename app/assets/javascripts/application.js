2718210ac95254dc876878fe08395bfa02306789
Ephraim Bismuth <ephraim_bismuth@hotmail.fr>
2018-08-16 15:47:57 +0300

    adding compulsory fields to sign up for goys

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,10 @@
 //= require rails-ujs
+//= require dependent-fields
+//= require underscore
+//= require jquery3
+//= require jquery_ujs
 //= require_tree .
+
+ $(document).ready(function() {
+     DependentFields.bind()
+ });
